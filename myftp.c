eeb54d5041fad3606c3a09c8b9034a0f31612416
msanc397 <71677615+msanc397@users.noreply.github.com>
2020-09-21 20:58:46 -0400

    Update myftp.c

--- myftp.c
+++ myftp.c
@@ -1,8 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <netdb.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h> 
 
 int main(int argc, char *argv[]) {
 	char* serverName;
+	char* IPAddress;
+	struct hostent* host;
+	
 	if (argc < 2 || argc > 2) {
 		printf("Usage: %s <server name>\nPlease try again and enter server name.\n", argv[0]);
 		exit(-1);
@@ -10,7 +19,14 @@ int main(int argc, char *argv[]) {
 	if (argc == 2) {
 		serverName = argv[1];
 	}
-	
+	host = gethostbyname(serverName);
+	if (host == NULL) {
+		printf("Server could not be found. Please try again.\n");
+		exit(-1);
+	}
+	IPAddress = inet_ntoa(*((struct in_addr*)host->h_addr_list[0])); 
+	printf("IP address = %s\n", IPAddress);
+  
 	printf("Server name is '%s'.\n", serverName);
 	return 0;
 }
