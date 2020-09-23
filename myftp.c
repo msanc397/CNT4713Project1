ff810ec10e61b5fe9c6998a64915a9012fa1787e
msanc397 <71677615+msanc397@users.noreply.github.com>
2020-09-23 16:37:00 -0400

    Update myftp.c

--- myftp.c
+++ myftp.c
@@ -6,14 +6,14 @@
 #include <sys/socket.h> 
 #include <netinet/in.h> 
 #include <arpa/inet.h> 
-#include <string.h>
 
 int main(int argc, char *argv[]) {
+	int sock, test, rec, peer;
 	char* serverName;
 	char* IPAddress;
 	struct hostent* host;
-	char userName[10];
-	char password[10];
+	struct sockaddr_in sockInfo;
+	char userName[10], password[10], serverMessage[250];
 	
 	if (argc < 2 || argc > 2) {
 		printf("Usage: %s <server name>\nPlease try again and enter server name.\n", argv[0]);
@@ -30,14 +30,33 @@ int main(int argc, char *argv[]) {
 	IPAddress = inet_ntoa(*((struct in_addr*)host->h_addr_list[0])); 
 	printf("IP address = %s\n", IPAddress);
   
-	printf("Server name is '%s'.\n", serverName);
-	
 	printf("Please enter username:\nUsername: ");
 	fgets(userName, 10, stdin);
-	strtok(userName, "\n");
 	printf("Please enter password:\nPassword: ");
 	fgets(password, 10, stdin);
-	strtok(password, "\n");
+	printf("Creating socket...\n");
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+		perror("Socket formation failed");
+		exit(-1);
+	}
 	
+	sockInfo.sin_family = AF_INET;
+	sockInfo.sin_port = htons(21);
+	
+	if ((test = (inet_pton(AF_INET, IPAddress, &(sockInfo.sin_addr)))) == 0) {
+		perror("Inet_pton failed");
+		exit(-1);
+	}
+	printf("Connecting...\n");
+	if (connect(sock, (struct sockaddr *)&sockInfo, sizeof(sockInfo)) < 0) {
+		perror("Connection failed");
+		exit(-1);
+	}
+	
+	printf("Connection successful.\n");
+	
+	rec = recv(sock, serverMessage, sizeof(serverMessage), 0);
+	
+	close(sock);
 	return 0;
 }
