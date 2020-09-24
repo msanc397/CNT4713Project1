2b49620e52a31b257f280b5e083a0a65dc1d7e0a
Bill Liza <billliza@Bills-MacBook-Pro.local>
2020-09-24 00:52:05 -0400

    replaced mywords with myftp in makefile

--- myftp.c
+++ myftp.c
@@ -6,19 +6,23 @@
 #include <sys/socket.h> 
 #include <netinet/in.h> 
 #include <arpa/inet.h> 
+#include <string.h>
+
+static char message_buffer[1024];
 
 int main(int argc, char *argv[]) {
-	int sock, test, rec, peer;
+	int sock, sock_receive, test, rec, len;
 	char* serverName;
 	char* IPAddress;
 	struct hostent* host;
 	struct sockaddr_in sockInfo;
-	char userName[10], password[10], serverMessage[250];
+	char userName[30], password[30], serverMessage[250], buffer[40], buffer2[40];
 	
 	if (argc < 2 || argc > 2) {
 		printf("Usage: %s <server name>\nPlease try again and enter server name.\n", argv[0]);
 		exit(-1);
 	}
+	
 	if (argc == 2) {
 		serverName = argv[1];
 	}
@@ -28,20 +32,16 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 	IPAddress = inet_ntoa(*((struct in_addr*)host->h_addr_list[0])); 
-	printf("IP address = %s\n", IPAddress);
-  
-	printf("Please enter username:\nUsername: ");
-	fgets(userName, 10, stdin);
-	printf("Please enter password:\nPassword: ");
-	fgets(password, 10, stdin);
+		
+	sockInfo.sin_family = AF_INET;
+	sockInfo.sin_port = htons(21);
+	
 	printf("Creating socket...\n");
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
 		perror("Socket formation failed");
 		exit(-1);
 	}
-	
-	sockInfo.sin_family = AF_INET;
-	sockInfo.sin_port = htons(21);
+	printf("Socket creation successful.\n");
 	
 	if ((test = (inet_pton(AF_INET, IPAddress, &(sockInfo.sin_addr)))) == 0) {
 		perror("Inet_pton failed");
@@ -52,10 +52,39 @@ int main(int argc, char *argv[]) {
 		perror("Connection failed");
 		exit(-1);
 	}
-	
 	printf("Connection successful.\n");
 	
+  
+	printf("Please enter username:\n");
+	scanf("%s", userName);
+	printf("Please enter password:\n");
+	scanf("%s", password);
+	
+	strcpy(buffer, "USER ");
+	strcat(buffer, userName);
+	strcat(buffer, "\r\n");
+	strcpy(userName, buffer);
+	strcpy(buffer, "");
+	strcpy(buffer, "PASS ");
+	strcat(buffer, password);
+	strcat(buffer, "\r\n");
+	strcpy(password, buffer);
+	
 	rec = recv(sock, serverMessage, sizeof(serverMessage), 0);
+	printf("Server reply: %.*s", rec, serverMessage);
+	
+	send(sock, userName, (int)strlen(userName), 0);
+	
+	rec = recv(sock, serverMessage, sizeof(serverMessage), 0);
+	printf("Server reply: %.*s", rec, serverMessage);
+	
+	send(sock, password, (int)strlen(password), 0);
+	
+	rec = recv(sock, serverMessage, sizeof(serverMessage), 0);
+	printf("Server reply: %.*s", rec, serverMessage);
+	
+	//continue here
+	
 	
 	close(sock);
 	return 0;
