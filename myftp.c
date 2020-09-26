83ac2b16a71cd617d550b46a030cb7f8b517ef68
camas002 <71781285+camas002@users.noreply.github.com>
2020-09-26 18:14:43 -0400

    trying to make ls work

--- myftp.c
+++ myftp.c
@@ -7,16 +7,49 @@
 #include <netinet/in.h> 
 #include <arpa/inet.h> 
 #include <string.h>
+#include <ctype.h>
 
-static char message_buffer[1024];
+static int sock;
+static int sock_data;
+static int rec;
+static char buffer[1024];
+static char serverMessage[1024];
+
+void myftp_quit() {
+	strcpy(buffer, "QUIT\r\n");
+	send(sock, buffer, strlen(buffer), 0);
+	strcpy(serverMessage, "");
+	rec = recv(sock, serverMessage, sizeof(serverMessage), 0);
+	printf("Server reply: %.*s", rec, serverMessage);
+	close(sock);
+}
+
+//format ip for port
+char* ipFormat(char* ipaddy) {
+	char delim[2] = ".";
+	char* token; 
+	char str[20] = "";
+	token = strtok(ipaddy, delim);
+	
+	while (token != NULL)
+	{
+		strcat(str, token);
+		if (token = strtok(NULL, delim))
+		{
+			strcat(str, ",");
+		}
+	}
+	
+}
 
 int main(int argc, char *argv[]) {
-	int sock, sock_receive, test, rec, len;
+	
+	int test, len;
 	char* serverName;
 	char* IPAddress;
 	struct hostent* host;
 	struct sockaddr_in sockInfo;
-	char userName[30], password[30], serverMessage[250], buffer[40], buffer2[40];
+	char userName[30], password[30], choice[20], fileDir[20];
 	
 	if (argc < 2 || argc > 2) {
 		printf("Usage: %s <server name>\nPlease try again and enter server name.\n", argv[0]);
@@ -82,10 +115,78 @@ int main(int argc, char *argv[]) {
 	
 	rec = recv(sock, serverMessage, sizeof(serverMessage), 0);
 	printf("Server reply: %.*s", rec, serverMessage);
+	getchar();
+	while (1) {
+		printf("myftp> ");
+		fgets(choice, 20, stdin);
+		strtok(choice, "\n");
+		
+		if (strcmp(choice, "quit") == 0) {
+			myftp_quit();
+			break;
+		}
+		else if (strcmp(choice, "ls") == 0) {
+			printf("ls method here\n");
+			strcpy(buffer, "PORT ");
+			//make ip in proper readable format.
+			char delim[2] = ".";
+			char* token;
+			char str[20] = "";
+			token = strtok(IPAddress, delim);
+			while (token != NULL)
+			{
+				strcat(str, token);
+				if (token = strtok(NULL, delim))
+				{
+					strcat(str, ",");
+				}
+			}
+			//append ip to port
+			strcat(buffer, str);
+			printf("%s", str);
+			puts("after ip");
+			strcat(buffer, ",2,1");
+			strcat(buffer, "\r\n");
+			puts("cat");
+			send(sock, buffer, strlen(buffer), 0);
+			puts("cat");
+			strcpy(serverMessage, "");
+			puts("cpy");
+			//rec = recv(sock, serverMessage, sizeof(serverMessage), 0);
+			puts("rec");
+			printf("Server reply: %.*s", rec, serverMessage);
+			strcpy(buffer, "LIST\r\n");
+			send(sock, buffer, strlen(buffer), 0);
+			strcpy(serverMessage, "");
+			rec = recv(sock, serverMessage, sizeof(serverMessage), 0);
+			printf("Server reply: %.*s", rec, serverMessage);
+
+		}	
+		else if (strncmp(choice, "cd ", 3) == 0) {
+			strncpy(fileDir, ((char*)choice)+3, strlen(choice)-1);
+			printf("File name: %s\n", fileDir);
+			//myftp_changedir(fileDir);
+		}
+		else if (strncmp(choice, "get ", 4) == 0) {
+			strncpy(fileDir, ((char*)choice)+4, strlen(choice)-1);
+			printf("File name: %s\n", fileDir);
+			//myftp_getfile(fileDir);
+		}
+		else if (strncmp(choice, "put ", 4) == 0) {
+			strncpy(fileDir, ((char*)choice)+4, strlen(choice)-1);
+			printf("File name: %s\n", fileDir);
+			//myftp_putfile(fileDir);
+		}
+		else if (strncmp(choice, "delete ", 7) == 0) {
+			strncpy(fileDir, ((char*)choice)+7, strlen(choice)-1);
+			printf("File name: %s\n", fileDir);
+			//myftp_deletefile(fileDir);
+		}
+		else {
+			printf("Invalid command.\n");
+		}
+		
+	}	
 	
-	//continue here
-	
-	
-	close(sock);
 	return 0;
 }
